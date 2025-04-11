bebddb4246c46aa68c11f7a09183988eefe410f4
nuyntai19 <tuantai1915@gmail.com>
2025-04-11 21:18:50 +0700

    upd sql

--- sql/DatabaseQLKS.java
+++ sql/DatabaseQLKS.java
@@ -4,6 +4,8 @@ package sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DatabaseQLKS {
@@ -14,4 +16,16 @@ public class DatabaseQLKS {
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+
+    public static void close(Connection conn, PreparedStatement stmt, ResultSet rs) {
+        try {
+            if(rs != null) rs.close();
+            if(stmt != null) stmt.close();
+            if(conn != null) conn.close();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+            
 }
