ff90a15693732efc4cc49cd23990baf4f665f8c3
Hee0207 <hy.vohoangphuc@gmail.com>
2025-04-28 14:26:42 +0700

    Add files via upload

---
+++ src/DTO/NhanVienDTO.java
@@ -0,0 +1,54 @@
+package DTO;
+
+public class NhanVienDTO {
+    private int maNhanVien;
+    private String ten;
+    private String ho;
+    private String ngaySinh;
+    private String gioiTinh;
+    private String email;
+    private String sdt;
+    private String chucVu;
+    private double luong;
+
+    public NhanVienDTO() {}
+
+    public NhanVienDTO(int maNhanVien, String ten, String ho, String ngaySinh, String gioiTinh, String email, String sdt, String chucVu, double luong) {
+        this.maNhanVien = maNhanVien;
+        this.ten = ten;
+        this.ho = ho;
+        this.ngaySinh = ngaySinh;
+        this.gioiTinh = gioiTinh;
+        this.email = email;
+        this.sdt = sdt;
+        this.chucVu = chucVu;
+        this.luong = luong;
+    }
+
+    public int getMaNhanVien() { return maNhanVien; }
+    public void setMaNhanVien(int maNhanVien) { this.maNhanVien = maNhanVien; }
+
+    public String getTen() { return ten; }
+    public void setTen(String ten) { this.ten = ten; }
+
+    public String getHo() { return ho; }
+    public void setHo(String ho) { this.ho = ho; }
+
+    public String getNgaySinh() { return ngaySinh; }
+    public void setNgaySinh(String ngaySinh) { this.ngaySinh = ngaySinh; }
+
+    public String getGioiTinh() { return gioiTinh; }
+    public void setGioiTinh(String gioiTinh) { this.gioiTinh = gioiTinh; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getSdt() { return sdt; }
+    public void setSdt(String sdt) { this.sdt = sdt; }
+
+    public String getChucVu() { return chucVu; }
+    public void setChucVu(String chucVu) { this.chucVu = chucVu; }
+
+    public double getLuong() { return luong; }
+    public void setLuong(double luong) { this.luong = luong; }
+}
