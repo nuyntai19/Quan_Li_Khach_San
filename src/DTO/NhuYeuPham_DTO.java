ad3916731adcc72d262d1f1f08176ff8bd23ccd1
Nguyen Minh Quang <nguyenminhquang2141@gmail.com>
2025-05-03 07:43:48 +0700

    Update NhuYeuPham_DTO.java

--- src/DTO/NhuYeuPham_DTO.java
+++ src/DTO/NhuYeuPham_DTO.java
@@ -1,52 +1,105 @@
 package DTO;
 
-import java.sql.Date;
+import java.util.Date;
 
-public class NhuYeuPham_DTO extends HangHoa_DTO {
-    private Date hanSuDung; 
+public class NhuYeuPham_DTO {
+    private String maHang;
+    private String tenHang;
+    private String donVi;
+    private double giaNhap;
+    private int soLuongTon;
+    private Date ngayNhap;
+    private Date hanSuDung;
     private String nhaCungCap;
-    private static final String LOAI_HANG = "NhuYeuPham"; 
+    private String tinhTrang;
 
     public NhuYeuPham_DTO() {
-        super();
-        this.hanSuDung = null;
-        this.nhaCungCap = "";
     }
 
-    // Constructor có tham số
-    public NhuYeuPham_DTO(int maHang, String tenHang, String donViTinh, double giaNhap, Date hanSuDung, String nhaCungCap) {
-        super(maHang, tenHang, donViTinh, giaNhap, LOAI_HANG);
+    public NhuYeuPham_DTO(String maHang, String tenHang, String donVi, double giaNhap, 
+                         int soLuongTon, Date ngayNhap, Date hanSuDung, String nhaCungCap, 
+                         String tinhTrang) {
+        this.maHang = maHang;
+        this.tenHang = tenHang;
+        this.donVi = donVi;
+        this.giaNhap = giaNhap;
+        this.soLuongTon = soLuongTon;
+        this.ngayNhap = ngayNhap;
         this.hanSuDung = hanSuDung;
         this.nhaCungCap = nhaCungCap;
+        this.tinhTrang = tinhTrang;
     }
 
-    // Constructor sao chép
-    public NhuYeuPham_DTO(NhuYeuPham_DTO tmp) {
-        super(tmp);
-        this.hanSuDung = tmp.hanSuDung;
-        this.nhaCungCap = tmp.nhaCungCap;
+    // Getters and Setters
+    public String getMaHang() {
+        return maHang;
     }
 
-    // Setter
+    public void setMaHang(String maHang) {
+        this.maHang = maHang;
+    }
+
+    public String getTenHang() {
+        return tenHang;
+    }
+
+    public void setTenHang(String tenHang) {
+        this.tenHang = tenHang;
+    }
+
+    public String getDonVi() {
+        return donVi;
+    }
+
+    public void setDonVi(String donVi) {
+        this.donVi = donVi;
+    }
+
+    public double getGiaNhap() {
+        return giaNhap;
+    }
+
+    public void setGiaNhap(double giaNhap) {
+        this.giaNhap = giaNhap;
+    }
+
+    public int getSoLuongTon() {
+        return soLuongTon;
+    }
+
+    public void setSoLuongTon(int soLuongTon) {
+        this.soLuongTon = soLuongTon;
+    }
+
+    public Date getNgayNhap() {
+        return ngayNhap;
+    }
+
+    public void setNgayNhap(Date ngayNhap) {
+        this.ngayNhap = ngayNhap;
+    }
+
+    public Date getHanSuDung() {
+        return hanSuDung;
+    }
+
     public void setHanSuDung(Date hanSuDung) {
         this.hanSuDung = hanSuDung;
+    }
+
+    public String getNhaCungCap() {
+        return nhaCungCap;
     }
 
     public void setNhaCungCap(String nhaCungCap) {
         this.nhaCungCap = nhaCungCap;
     }
 
-    // Getter
-    public Date getHanSuDung() {
-        return this.hanSuDung;
+    public String getTinhTrang() {
+        return tinhTrang;
     }
 
-    public String getNhaCungCap() {
-        return this.nhaCungCap;
-    }
-
-    @Override
-    public String getLoaiHang() {
-        return LOAI_HANG;
+    public void setTinhTrang(String tinhTrang) {
+        this.tinhTrang = tinhTrang;
     }
 }
